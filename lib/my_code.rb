10c1060b1fe5c07f8ff3340d8c76213b69b22a57
Dakota Willingham <kota@vaga.tk>
2020-02-27 18:52:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,12 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  counter = 0
+  while counter < source_array.length do
+    source_array[counter] *= 2
+    counter +=1
+  end
+  source_array
+end
