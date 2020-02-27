7515681bc4be43ff903d156359e0d2123cfb6656
Dakota Willingham <kota@vaga.tk>
2020-02-27 18:54:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,5 +30,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  counter = starting_point
+  total = 0
+  while counter < source_array.length do
+    total += source_array[counter]
+    counter += 1
+  end
+  total
 end
