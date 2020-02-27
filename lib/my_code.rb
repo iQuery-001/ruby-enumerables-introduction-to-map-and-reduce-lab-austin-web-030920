1e21e266f85183be0e8bddb785ae610d919762e7
Dakota Willingham <kota@vaga.tk>
2020-02-27 19:06:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,12 +52,13 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  all_true = false
+  any_true = false
   counter = 0
   while counter < source_array.length do
     if source_array[counter]
-      all_true = true
+      any_true = true
     end
     counter += 1
   end
+  any_true
 end
